dd2861e03c0c93d8faefadae8e7e3cd5af74e499
Shubham <shuso822@gmail.com>
2022-12-13 01:28:27 +0530

    Minor changes in styling and result page

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -8,22 +8,24 @@ const Result = () => {
     console.log(result);
     Array.from(data).forEach(item => {
         if (item.mealplan === 1) {
+            let meeaal = "Breakfast Included";
             let couple = item.couples * item.hotel[0].MAP_double;
             let adult = item.adult * item.hotel[0].Adult_MAP;
             let child = item.child * item.hotel[0].Child_MAP;
             let hotelName = item.hotel[0].Hotel_Name;
             let calculation = (couple + adult + child) * item.nights;
 
-            const arr = { couple: couple, adult: adult, child: child, hotelName: hotelName, result: calculation, nights : item.nights , d1 : item.d1 , d2 : item.d2 };
+            const arr = { meeaal: meeaal, couple: couple, adult: adult, child: child, hotelName: hotelName, result: calculation, nights : item.nights , d1 : item.d1 , d2 : item.d2 };
             result.push(arr);
         } else {
+            let meeaal = "Breakfast Excluded";
             let couple = item.couples * item.hotel[0].CP_double;
             let adult = item.adult * item.hotel[0].Adult_CP;
             let child = item.child * item.hotel[0].Child_CP;
             let hotelName = item.hotel[0].Hotel_Name;
             let calculation = (couple + adult + child) * item.nights;
 
-            const arr = { couple: couple, adult: adult, child: child, hotelName: hotelName, result: calculation , nights : item.nights , d1 : item.d1 , d2 : item.d2};
+            const arr = { meeaal: meeaal, couple: couple, adult: adult, child: child, hotelName: hotelName, result: calculation , nights : item.nights , d1 : item.d1 , d2 : item.d2};
             result.push(arr);
         }
     })
@@ -50,7 +52,7 @@ const Result = () => {
             <h4>
               Greetings from 'Adwait Tours', your tour details are:
             </h4>
-
+        
             <div>No. of couples:{data[0].couples}</div>   
             <div>Extra adult   :{data[0].adult}</div>
             <div>No. of childs :{data[0].child}</div>
@@ -63,19 +65,21 @@ const Result = () => {
                             <div>&#128197;Check In            :{item.d1}</div>
                             <div>&#128197;Check Out           :{item.d2}</div>
                             <div>&#127747;Nights              :{item.nights}</div>
-                            <div>&#128176;Couple charges      :{item.couple}</div>
-                            <div>&#128176;Extra adults charges:{item.adult}</div>
-                            <div>&#128176;Childs charges      :{item.child}</div>
-                            {/* <div>&#127857;Meal Plan           :</div> */}
+                            <div>&#128176;Couple charges      :&#8377;{item.couple}/-</div>
+                            <div>&#128176;Extra adults charges:&#8377;{item.adult}/-</div>
+                            <div>&#128176;Childs charges      :&#8377;{item.child}/-</div>
+                            <div>&#127857;Meal Plan           :{item.meeaal}</div>
                             <br />
                         </>
                     )
                 })
             }
             <br />
-            &#128181; Total Tour Package: {total}
+            &#128181; Total Tour Package:&#8377; {total}/- only
+            <br /><br />
+            Thank You!
         </div>
-        <button onClick={()=>{copytext()}}>copy text</button>
+        <button class="coppee" onClick={()=>{copytext()}}>Copy Text</button>
         </>
     )
 }
